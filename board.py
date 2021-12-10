ffb1012d0be92524079525d93d06b663ef38f0a7
JP <jcproctor@my.waketech.edu>
2021-12-09 23:25:18 -0500

    added win conditions and check for draw

--- board.py
+++ board.py
@@ -4,6 +4,74 @@ def print_board(board):
     print(board[2])
 
 
+def end_game_p1(board):
+    is_won = False
+
+    # check rows for three 1's
+    rows = [0, 1, 2]
+    for i in rows:
+        if board[i][0] == '1' and board[i][1] == '1' and board[i][2] == '1':
+            is_won = True
+            print('Player 1 won!')
+            break
+
+    # If player did not win on rows, try columns
+    if is_won == False:
+        cols = [0, 1, 2]
+        for i in cols:
+            if board[0][i] == '1' and board[1][i] == '1' and board[2][i] == '1':
+                is_won = True
+                print('Player 1 won!')
+                break
+
+    # If columns did not win, try the diagonals
+    if is_won == False:
+        if board[0][0] == '1' and board[1][1] == '1' and board[2][2] == '1':
+            is_won = True
+            print('Player 1 won!')
+
+    if is_won == False:
+        if board[0][2] == '1' and board[1][1] == '1' and board[2][0] == '1':
+            is_won = True
+            print('Player 1 won!')
+
+    return is_won
+
+
+def end_game_p2(board):
+    is_won = False
+
+    # check rows for three 1's
+    rows = [0, 1, 2]
+    for i in rows:
+        if board[i][0] == '2' and board[i][1] == '2' and board[i][2] == '2':
+            is_won = True
+            print('Player 2 won!')
+            break
+
+    # If player did not win on rows, try columns
+    if is_won == False:
+        cols = [0, 1, 2]
+        for i in cols:
+            if board[0][i] == '2' and board[1][i] == '2' and board[2][i] == '2':
+                is_won = True
+                print('Player 2 won!')
+                break
+
+    # If columns did not win, try the diagonals
+    if is_won == False:
+        if board[0][0] == '2' and board[1][1] == '2' and board[2][2] == '2':
+            is_won = True
+            print('Player 2 won!')
+
+    if is_won == False:
+        if board[0][2] == '2' and board[1][1] == '2' and board[2][0] == '2':
+            is_won = True
+            print('Player 2 won!')
+
+    return is_won
+
+
 # create board
 list1 = ['-', '-', '-']
 list2 = ['-', '-', '-']
@@ -42,6 +110,22 @@ while isPlaying:
 
     print_board(board)
 
+    # See if p1 won
+    game_over = end_game_p1(board)
+
+    # Check for a draw
+    if not game_over:
+        board_full = True
+        for i in board:
+            if '-' in i:
+                board_full = False
+        if board_full:
+            print('Draw')
+            game_over = True
+
+    if game_over:
+        break
+
     # player 2 input
     isValid = False
     while not isValid:
@@ -66,7 +150,22 @@ while isPlaying:
             print("Space Already Taken\n")
 
     board[row][column] = "2"
-
     print_board(board)
+
+    # See if p2 won
+    game_over = end_game_p1(board)
+
+    # Check for a draw
+    if not game_over:
+        board_full = True
+        for i in board:
+            if '-' in i:
+                board_full = False
+        if board_full:
+            print('Draw')
+            game_over = True
+
+    if game_over:
+        break
 
 
